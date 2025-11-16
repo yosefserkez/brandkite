31b88238ad75bc4a5333629e12a128a745528af3
Yosef Serkez <yosefserkez@gmail.com>
2025-11-16 12:31:33 -0500

    Refactor Logo component to handle URL rendering with img tag for better CORS support; simplify InlineSVG logic by removing unnecessary state management and effects.

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-
 type LogoProps = { svg: string; url?: never } | { svg?: never; url: string };
 
 export default function Logo(props: LogoProps) {
@@ -7,58 +5,30 @@ export default function Logo(props: LogoProps) {
 		return <InlineSVG svg={props.svg} />;
 	}
 	if (props.url) {
-		return <InlineSVG url={props.url} />;
+		// Use img tag for URLs to avoid CORS issues (browsers handle img CORS more leniently)
+		return (
+			<div className="logo h-full w-full">
+				<img
+					alt="Logo"
+					className="h-full w-full object-contain"
+					height="100"
+					src={props.url}
+					width="100"
+				/>
+			</div>
+		);
 	}
 	return null;
 }
 
-type InlineSVGProps =
-	| { svg: string; url?: never }
-	| { svg?: never; url: string };
+type InlineSVGProps = { svg: string };
 
 function InlineSVG(props: InlineSVGProps) {
-	const [svg, setSvg] = useState(props.svg ?? "");
-
-	useEffect(() => {
-		if (props.svg) {
-			setSvg(props.svg);
-			return;
-		}
-		if (!props.url) {
-			return;
-		}
-		const HTTP_OK_STATUS = 200;
-		fetch(props.url)
-			.then((res) => {
-				if (res.status !== HTTP_OK_STATUS) {
-					return;
-				}
-				return res.text();
-			})
-			.then((text) => {
-				if (text !== undefined) {
-					setSvg(text);
-				}
-			})
-			.catch((error) => {
-				throw error;
-			});
-	}, [props.svg, props.url]);
-
 	return (
-		<>
-			{svg ? (
-				// biome-ignore lint/security/noDangerouslySetInnerHtml: needed to override fill and height/width
-				<div
-					className="logo h-full w-full"
-					// biome-ignore lint/security/noDangerouslySetInnerHtml: needed to set the SVG
-					dangerouslySetInnerHTML={{ __html: svg }}
-				/>
-			) : (
-				<div className="flex h-full w-full items-center justify-center bg-gray-100">
-					<span className="font-bold text-4xl text-gray-400">?</span>
-				</div>
-			)}
-		</>
+		<div
+			className="logo h-full w-full"
+			// biome-ignore lint/security/noDangerouslySetInnerHtml: needed to set the SVG
+			dangerouslySetInnerHTML={{ __html: props.svg }}
+		/>
 	);
 }
